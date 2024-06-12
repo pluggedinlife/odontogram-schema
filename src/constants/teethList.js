9fbeaac59a3104332feca4a7092f868baac33450
Mikhail Nazaretyan @pluggedinlife <sgas5570@gmail.com>
2024-06-12 10:44:42 +0200

    Enhance the constants' structure

--- src/constants/teethList.js
+++ src/constants/teethList.js
@@ -5,136 +5,168 @@ export default {
     {
       id: 0,
       uns: '1',
+      fdi: '18',
     },
     {
       id: 1,
       uns: '2',
+      fdi: '17',
     },
     {
       id: 2,
       uns: '3',
+      fdi: '16',
     },
     {
       id: 3,
       uns: '4',
+      fdi: '15',
     },
     {
       id: 4,
       uns: '5',
+      fdi: '14',
     },
     {
       id: 5,
       uns: '6',
+      fdi: '13',
     },
     {
       id: 6,
       uns: '7',
+      fdi: '12',
     },
     {
       id: 7,
       uns: '8',
+      fdi: '11',
     },
 
     // UPPER LEFT
     {
       id: 8,
       uns: '9',
+      fdi: '21',
     },
     {
       id: 9,
       uns: '10',
+      fdi: '22',
     },
     {
       id: 10,
       uns: '11',
+      fdi: '23',
     },
     {
       id: 11,
       uns: '12',
+      fdi: '24',
     },
     {
       id: 12,
       uns: '13',
+      fdi: '25',
     },
     {
       id: 13,
       uns: '14',
+      fdi: '26',
     },
     {
       id: 14,
       uns: '15',
+      fdi: '27',
     },
     {
       id: 15,
       uns: '16',
+      fdi: '28',
     },
 
     // LOWER LEFT
     {
       id: 16,
       uns: '17',
+      fdi: '38',
     },
     {
       id: 17,
       uns: '18',
+      fdi: '37',
     },
     {
       id: 18,
       uns: '19',
+      fdi: '36',
     },
     {
       id: 19,
       uns: '20',
+      fdi: '35',
     },
     {
       id: 20,
       uns: '21',
+      fdi: '34',
     },
     {
       id: 21,
       uns: '22',
+      fdi: '33',
     },
     {
       id: 22,
       uns: '23',
+      fdi: '32',
     },
     {
       id: 23,
       uns: '24',
+      fdi: '31',
     },
 
     // LOWER RIGHT
     {
       id: 24,
       uns: '25',
+      fdi: '41',
     },
     {
       id: 25,
       uns: '26',
+      fdi: '42',
     },
     {
       id: 26,
       uns: '27',
+      fdi: '43',
     },
     {
       id: 27,
       uns: '28',
+      fdi: '44',
     },
     {
       id: 28,
       uns: '29',
+      fdi: '45',
     },
     {
       id: 29,
       uns: '30',
+      fdi: '46',
     },
     {
       id: 30,
       uns: '31',
+      fdi: '47',
     },
     {
       id: 31,
       uns: '32',
+      fdi: '48',
     },
   ],
   primary: [

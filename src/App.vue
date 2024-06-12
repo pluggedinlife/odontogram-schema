f14f5b7de97b52d249feb10535631b4002d987d5
Mikhail Nazaretyan @pluggedinlife <sgas5570@gmail.com>
2024-06-12 10:45:18 +0200

    Minor code enhancements: extend the props

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import OdontogramViewer from './components/OdontogramViewer.vue';
-
-const odontogramTitle = 'Odontogram';
-const isPermanent = false;
 </script>
 
 <template>
-  <OdontogramViewer
-    :devMode="true"
-    :title="odontogramTitle"
-    :isPermanent="isPermanent"
-  />
+  <div class="p-4">
+    <OdontogramViewer
+      :devMode="true"
+      title="Odontogram"
+      :isPermanent="true"
+      notation="fdi"
+    />
+  </div>
 </template>
--- src/components/OdontogramViewer.vue
+++ src/components/OdontogramViewer.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { onRenderTracked } from 'vue';
 import teethList from '../constants/teethList';
+import ElementSchema from './ElementSchema.vue';
 
 defineProps({
   devMode: Boolean,
   title: String,
   isPermanent: Boolean,
+  notation: String, // uns / fdi
 });
 
 let upperArch = [];
@@ -31,33 +33,55 @@ function resetLocalData() {
 </script>
 
 <template>
-  <div class="flex flex-col" :class="devMode ? 'bg-blue-100 ' : ''">
-    <div class="m-5 font-semibold">
+  <div
+    class="flex flex-col border border-gray-500 rounded-lg overflow-auto"
+    :class="devMode ? 'bg-blue-100 ' : ''"
+  >
+    <div class="m-4 font-semibold" :class="devMode ? 'bg-orange-100 ' : ''">
       <span>{{ title }}</span>
     </div>
 
-    <div class="flex flex-col min-w-[800px]">
+    <div class="flex flex-col items-center min-w-[1200px]">
       <!-- UPPER ARCH -->
-      <div class="flex flex-row-reverse">
-        <div
-          class="border border-gray-500 h-40 w-20"
-          v-for="(element, index) in upperArch"
-          :key="index"
-        >
-          {{ element.uns }}
+      <div>
+        <div class="w-full flex justify-between">
+          <span>LX</span>
+          <span>RX</span>
+        </div>
+
+        <div class="flex flex-row-reverse">
+          <ElementSchema
+            class="border border-gray-500 h-40 w-20"
+            v-for="(element, index) in upperArch"
+            :key="index"
+            :element="element"
+            :isPermanent="isPermanent"
+            :notation="notation"
+            :devMode="devMode"
+          />
         </div>
       </div>
 
       <div class="border border-b mx-2 border-gray-400 my-4" />
 
       <!-- LOWER ARCH -->
-      <div class="flex">
-        <div
-          class="border border-gray-500 h-40 w-20"
-          v-for="(element, index) in lowerArch"
-          :key="index"
-        >
-          {{ element.uns }}
+      <div>
+        <div class="flex">
+          <ElementSchema
+            class="border border-gray-500 h-40 w-20"
+            v-for="(element, index) in lowerArch"
+            :key="index"
+            :element="element"
+            :isPermanent="isPermanent"
+            :notation="notation"
+            :devMode="devMode"
+            :isReverse="true"
+          />
+        </div>
+
+        <div class="w-full flex justify-between">
+          <span>LX</span>
+          <span>RX</span>
         </div>
       </div>
     </div>

2eef36cf798b56f89123b5862bce586519c2951d
Mikhail Nazaretyan @pluggedinlife <sgas5570@gmail.com>
2024-06-14 11:24:08 +0200

    Minor enhancements: extend the objects' structure to host roots

--- src/constants/teethList.js
+++ src/constants/teethList.js
@@ -6,41 +6,117 @@ export default {
       id: 0,
       uns: '1',
       fdi: '18',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 1,
       uns: '2',
       fdi: '17',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 2,
       uns: '3',
       fdi: '16',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 3,
       uns: '4',
       fdi: '15',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 4,
       uns: '5',
       fdi: '14',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 5,
       uns: '6',
       fdi: '13',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 6,
       uns: '7',
       fdi: '12',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 7,
       uns: '8',
       fdi: '11',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
 
     // UPPER LEFT
@@ -48,41 +124,117 @@ export default {
       id: 8,
       uns: '9',
       fdi: '21',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 9,
       uns: '10',
       fdi: '22',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 10,
       uns: '11',
       fdi: '23',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 11,
       uns: '12',
       fdi: '24',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 12,
       uns: '13',
       fdi: '25',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 13,
       uns: '14',
       fdi: '26',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 14,
       uns: '15',
       fdi: '27',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 15,
       uns: '16',
       fdi: '28',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
 
     // LOWER LEFT
@@ -90,41 +242,101 @@ export default {
       id: 16,
       uns: '17',
       fdi: '38',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 17,
       uns: '18',
       fdi: '37',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 18,
       uns: '19',
       fdi: '36',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 19,
       uns: '20',
       fdi: '35',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 20,
       uns: '21',
       fdi: '34',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 21,
       uns: '22',
       fdi: '33',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 22,
       uns: '23',
       fdi: '32',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 23,
       uns: '24',
       fdi: '31',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
 
     // LOWER RIGHT
@@ -132,75 +344,102 @@ export default {
       id: 24,
       uns: '25',
       fdi: '41',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 25,
       uns: '26',
       fdi: '42',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 26,
       uns: '27',
       fdi: '43',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 27,
       uns: '28',
       fdi: '44',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 28,
       uns: '29',
       fdi: '45',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 29,
       uns: '30',
       fdi: '46',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 30,
       uns: '31',
       fdi: '47',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
     {
       id: 31,
       uns: '32',
       fdi: '48',
+      roots: [
+        {
+          id: 0,
+          name: '1',
+        },
+        {
+          id: 0,
+          name: '1',
+        },
+      ],
     },
   ],
-  primary: [
-    {
-      id: 0,
-      uns: '1',
-    },
-    {
-      id: 1,
-      uns: '2',
-    },
-    {
-      id: 2,
-      uns: '3',
-    },
-    {
-      id: 3,
-      uns: '4',
-    },
-    {
-      id: 4,
-      uns: '5',
-    },
-    {
-      id: 5,
-      uns: '6',
-    },
-    {
-      id: 6,
-      uns: '7',
-    },
-    {
-      id: 7,
-      uns: '8',
-    },
-  ],
+  primary: [],
 };
